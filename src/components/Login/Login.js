04537f12ec9d92ba8aa25193f9f9a7366603b05c
sinamong0620 <sinaskim60@gmail.com>
2023-07-19 00:55:35 +0900

    how to use UseReducer
    UseReducer에 대해서 학습함.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,40 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+//emailReducer 함수는 컴포넌트 함수 내부에서 만들어진 어떤 데이터도 필요하지 않기 때문이다. 따라서 이 리듀서 함수는 컴포넌트 함수의 범위 밖에서 만들어 질 수 있다.
+//값과 유효성을 하나의 state로 결합하여 useReducer로 관리하는 것.
+const emailReducer = (state, action) => {
+  console.log(state);
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
-    }, 500);
+  //useReducer 함수의 첫번째 인자는 함수를 취한다.
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: undefined,
+  });
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
 
-    return () => {
-      console.log("클린업 함수");
-      clearTimeout(identifier);
-    }; //이것은 클린업 프로세스이다. 실행되기 전에 이 클린업 함수가 실행이 된다. 처음 실행되는 때를 제외하고. 그리고 컴포넌트가 제거되기 전에 실행된다.
-    //첫번째 사이드이펙트 함수가 실행되기 전에는 실행되지 않는다.
-  }, [setFormIsValid, enteredEmail, enteredPassword]);
+  //   return () => {
+  //     console.log("클린업 함수");
+  //     clearTimeout(identifier);
+  //   }; //이것은 클린업 프로세스이다. 실행되기 전에 이 클린업 함수가 실행이 된다. 처음 실행되는 때를 제외하고. 그리고 컴포넌트가 제거되기 전에 실행된다.
+  //   //첫번째 사이드이펙트 함수가 실행되기 전에는 실행되지 않는다.
+  // }, [setFormIsValid, enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    dispatchEmail({ type: "INPUT_BLUR" });
+    // setEmailIsValid(emailState.isValid);
+    //다른 state값으로 state값을 설정해서는 안되는 일이다.
+    //이런 경우에는 하나의 state로 병합하는 것도 좋다.
   };
 
   const validatePasswordHandler = () => {
@@ -43,7 +69,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -51,14 +77,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
